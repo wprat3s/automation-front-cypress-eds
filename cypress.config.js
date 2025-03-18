81c2cf3a6025662e3ecdf565281e12c8767625e2
William Prates <90214236+wprat3s@users.noreply.github.com>
2025-03-17 21:26:17 -0300

    Alterando configurações para tentar abrir o navegador ao rodar o cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,30 @@
-const { defineConfig } = require("cypress");
+//const { defineConfig } = require("cypress");
+
+//module.exports = defineConfig({
+  //e2e: {
+    //baseUrl:'http://front.serverest.dev',
+    //chromeWebSecurity: false,
+    //experimentalSessionAndOrigin: true,
+    //setupNodeEvents(on, config) {
+      // implement node event listeners here.
+    //},
+  //},
+//});
+
+//const { defineConfig } = require('cypress');
+
+//module.exports = defineConfig({
+ // e2e: {
+   // baseUrl: 'https://front.serverest.dev/login', // URL do site serverest pata o
+   // chromeWebSecurity: false, // Desativa verificação de segurança no Chrome
+ // },
+//});
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl:'http://front.serverest.dev',
-    chromeWebSecurity: false,
-    experimentalSessionAndOrigin: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here.
-    },
-  },
+    baseUrl: 'https://front.serverest.dev',
+    chromeWebSecurity: false, 
+    experimentalModifyObstructiveThirdPartyCode: true, // Tenta ignorar bloqueios de segurança
+  }
 });
